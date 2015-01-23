489e25190b15932c19ea5d241d80566f07cc0abf
drwasho <washington.sanchez@gmail.com>
2015-01-23 16:16:11 +1000

    Nearly there
    + Got to get that button working
    + Removed a lot of code that I'll refactor and add back in

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,22 +13,30 @@ part1.controller('Working',
         $scope.start = function () {
             var itemTitle = item_title.value;
             var itemDescription = item_description.value;
-
-            var dudeden = JSON.stringify({stage01_merchant: {item_title: itemTitle, item_description: itemDescription}});
+            var itemPrice = item_price.value;
+            var itemFiatPrice = item_fiat_price.value;
+            var itemFiatXE = item_fiat_xe.value;
+            var itemImage = item_image.value;
+            var itemImageHash = item_image_hash.value;
+            var itemCondition = item_condition.value;
+            var itemQuantity = item_quantity.value;
+            var itemKeywords = item_keywords.value;
+            var itemRegion = item_region.value;
+            var itemEstDelivery = item_estdelivery.value;
+            var metadataExpDate = metadata_expdate.value;
+            var merchantGUID = merchant_guid.value;
+            var merchantHandle = merchant_handle.value;
+            var merchantLegalAddress = merchant_legaladdress.value;
+            var merchantPGPPubkey = merchant_pgp_pubkey.value;
+            var merchantECPubkey = merchant_secp256k1_pubkey.value;
+            var merchantDigiSig = merchant_digisig.value;
+            var dudeden = JSON.stringify({ stage01_merchant: { metadata: {obcv: "0.0.2", category: "Physical item", subcategory: "Fixed Price", Expiration_date: metadataExpDate}, item_data: {title: itemTitle, description: itemDescription, price: {btc: itemPrice, fiat: {price: itemFiatPrice, currency: itemFiatXE}}, image_link: itemImage, image_hash: itemImageHash, condition: itemCondition, quantity: itemQuantity, keywords: itemKeywords, region: itemRegion, estimated_delivery: itemEstDelivery}, merchant: {guid: merchantGUID, handle: merchantHandle, legal_address: merchantLegalAddress, pgp_pubkey: merchantPGPPubkey, bitcoin_pubkey: merchantECPubkey} }, signatures: {merchant_sig: merchantDigiSig}}, null, "\t");
             $scope.display1 = dudeden;
             $scope.newcontract = JSON.parse(dudeden);
-            //$scope.newcontract = JSON.parse($("#createcontract").text());
-            //console.log($scope.newcontract);
             $scope.stage01hash = sha256_digest($("#merchanthash").text());
-            console.log($scope.stage01hash);
-            $scope.stage02hash = sha256_digest($("#buyerhash").text());
             console.log($scope.stage01hash);
         };
         $scope.dasverify = function () {
-            openpgp.initWorker = function (s) {
-                openpgp.initWorker("openpgp.worker.min.js");
-            };
-            // Stop, hammer time
             var daspublickey = $("#daspubkey").text();
             var publickey = openpgp.key.readArmored(daspublickey).keys[0];
             console.log(publickey);
@@ -36,7 +44,7 @@ part1.controller('Working',
             var signature = openpgp.cleartext.readArmored(dassignature);
             console.log(signature);
             openpgp.verifyClearSignedMessage(publickey, signature).then(function(sigCheck) {
-                console.log(sigCheck.signatures[0].valid); // success
+                $scope.check = sigCheck.signatures[0].valid;
             });
         };
     });
